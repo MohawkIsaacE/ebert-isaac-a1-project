7573897a5ee7cac7be3769a4fa76d0125274da45
Isaac Ebert <000960242@mohawkcollege.ca>
2024-09-18 11:10:51 -0400

    Add title screen

--- ebert-isaac-a1-rpg/Program.cs
+++ ebert-isaac-a1-rpg/Program.cs
@@ -1,2 +1,27 @@
-﻿// Assignment 1: Interactive Terminal Project
+﻿// Game Development Foundations
+// Assignment 1: Interactive Terminal Project
 // Isaac Ebert
+
+// Variables
+bool hasSword = false;
+bool hasRedGem = false;
+bool hasOrangeGem = false;
+bool hasYelloGem = false;
+bool hasGreenGem = false;
+bool hasBlueGem = false;
+bool hasPurpleGem = false;
+int currency = 0;
+
+// Title screen and controls display
+Console.WriteLine("Controls: Type the number of the option you want to choose, then press enter.");
+Console.WriteLine("1. Play\n2. Quit");
+
+int initialOption = int.Parse(Console.ReadLine());
+if (initialOption == 2)
+{
+    Console.WriteLine("You decide not to play.");
+}
+else
+{
+    Console.WriteLine("Ooption1");
+}
